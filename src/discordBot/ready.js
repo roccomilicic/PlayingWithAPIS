786b8557df379cc0baf56a8f35418d81a5d9123a
roccomilicic <roccomilicic2@gmail.com>
2024-06-11 14:33:55 +1200

    intents now working for discord bot

--- src/discordBot/ready.js
+++ src/discordBot/ready.js
@@ -1,21 +1,13 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, IntentsBitField } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
 });
-
-client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
-
 
 client.login(token);
-
-client.on('channelCreate', async channel => {
-    // Check if the channel is a text channel and matches the name format "ticket-xxxx"
-    if (channel.type === 'GUILD_TEXT' && /^ticket-\d+$/.test(channel.name)) {
-        // Send a greeting message in the newly created channel
-        await channel.send('Hello! How can I assist you?');
-    }
-});
