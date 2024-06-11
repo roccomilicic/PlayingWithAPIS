10221593fbfaee5040e92269e1fd19dc04ee0485
roccomilicic <roccomilicic2@gmail.com>
2024-06-11 15:36:36 +1200

    Bot now replies to new channel created

--- src/discordBot/ready.js
+++ src/discordBot/ready.js
@@ -1,4 +1,4 @@
-const { Client, IntentsBitField } = require('discord.js');
+const { Client, IntentsBitField, InteractionResponse } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({
@@ -7,6 +7,7 @@ const client = new Client({
         IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildMessages,
     ]
 });
 
@@ -14,8 +15,20 @@ client.on('ready', (c) => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('messageCreate', (msg) => {
+    // If the message is "ping"
+    if (msg.content === 'ping') {
+        // Send "pong" to the same channel
+        msg.channel.send('pong');
+    }
+});
+
 client.on('channelCreate', (channel) => {
-    console.log(`Channel created: ${channel.name}`);
+    // Check if the created channel is a text channel
+    if (channel.type === 0) { // 0 represents GUILD_TEXT in the discord.js v13 and later
+        // Send a message to the newly created channel
+        channel.send('A new text channel has been created!');
+    }
 });
 
 client.login(token);
