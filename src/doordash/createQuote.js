b2ab5d052aa64dca97de76009a21968a2a112f5a
roccomilicic <roccomilicic2@gmail.com>
2024-06-16 23:07:50 +1200

    refactor: Update createQuote function to use DoorDashClient for delivery quotes

--- src/doordash/createQuote.js
+++ src/doordash/createQuote.js
@@ -1,28 +1,22 @@
 const axios = require('axios');
+const DoorDashClient = require('@doordash/sdk');
 const accessKey = require('./accesskey.js');
 const { uuid } = require('uuidv4');
 
 async function createQuote(token, { pickupAddress, pickupPhoneNumber, dropoffAddress, dropoffPhoneNumber, orderValue }) {
-    const url = 'https://openapi.doordash.com/drive/v2/quotes';
-
-    const data = {
-        external_delivery_id: uuid(),
-        pickup_address: pickupAddress,
-        dropoff_address: dropoffAddress,
-        dropoff_phone_number: '+62288513174',
-    };
-
-    const headers = {
-        'Authorization': 'Bearer ' + token,
-        'Content-Type': 'application/json',
-    };
-
+    const client = new DoorDashClient.DoorDashClient(accessKey);
+    
     try {
-        const response = await axios.post(url, data, { headers });
-        console.log("Response for quote:\n", response.data);
+        const response = await client.deliveryQuote({
+            external_delivery_id: uuid(),
+            pickup_address: pickupAddress,
+            dropoff_address: dropoffAddress,
+            dropoff_phone_number: '+62288513174',
+        });
+        console.log("Response for acceptQuote:\n", response.message);
         return response.data;
     } catch (error) {
-        throw new Error('Error creating quote: ' + error.field_errors);
+        throw new Error('\nError accepting delivery: ' + error.message);
     }
 }
 
